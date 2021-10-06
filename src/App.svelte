a6ef5606f81484d3c7304fd1a20e628ad3948109
reaper-king <30295592+reaper-king@users.noreply.github.com>
2021-10-06 17:24:12 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,63 @@
-<script>
-	export let name;
+<script> 
+import { onMount } from 'svelte';
+	import gsap from 'gsap'
+import Heart from './heart.svelte'
+	
+import { pumpHeart } from './heart.js';
+	
+	onMount(()=> pumpHeart(0) )
+	
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Heart></Heart>
+<Heart></Heart>
+<Heart></Heart>
+<Heart></Heart>
+<Heart></Heart>		
+	let pumpLevels = [5, 10, 25, 0];
+  let tankLevels = [4,14, 70, -20];
+  let isAnimating = false;
+  export function pumpHeart(counter) {
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+    if(isAnimating) {
+      return;
+    }
+    
+    isAnimating = true;
+    //forward
+    gsap.to('.heart', {
+      translateZ: pumpLevels[counter], 
+      duration: 0.5
+    });
+    
+    gsap.to(".curve", {
+      bottom: tankLevels[counter],
+      transformOrigin: "bottom",
+      scaleY: 1,
+      duration: 0.5
+    })
+      gsap.to(".tank", {
+      height: counter === 3 ? 0 : tankLevels[counter],
+      duration: 0.5
+    })
+    
+    //reverse
+    gsap.to(".curve", {
+      delay: 0.6,
+      bottom: tankLevels[counter],
+      transformOrigin: "bottom",
+      scaleY: 0.5,
+      duration: 0.5
+    });
+    
+    gsap.to('.heart', {
+      delay: 0.6,
+      translateZ: 0,
+      duration: 0.25,
+      onComplete: function() {
+        isAnimating = false;
+      }
+    })
+    
+    if(++counter > 3) counter = 0;
+  }
