e658614b403134f2ed40a26d27298b80c032f47b
reaper-king <30295592+reaper-king@users.noreply.github.com>
2021-10-10 17:12:08 +0200

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,76 @@
+<!-- <script>
+
+import { onMount } from  'svelte';
+import Heart_1 from './heart.svelte';
+ 
+let hearts 
+//  = [0,1,2,3,4,5,6,7]
+
+
+
+async function fetchMoviesJSON() {
+                const response = await fetch('https://gs-api.vercel.app/bl');
+                const sheetData = await response.json();
+                return sheetData;
+            }
+
+            fetchMoviesJSON().then(sheetData => {
+
+              hearts = sheetData[0]
+            });
+
+
+/*
+
+0 - 0
+1 - 10
+2 - 20 -30 
+3 - 40
+4 -50
+5 = 50 60 
+6 70 80
+7 90 
+8 100
+
+*/
+
+</script>
+<ul>
+
+{#if hearts}
+{#each hearts as heart , i }
+<li>
+
+  <Heart_1 count={heart} count_id={i}></Heart_1>
+
+</li>
+{/each}
+{/if}
+</ul>
+
+
+<style>
+    
+	ul {
+        display: -webkit-box;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;  
+        -webkit-flex-flow: row wrap;
+        flex-flow: row wrap;
+        gap:-0.5em;
+          align-items: start;
+          justify-content:center;
+		  width: 100%;
+	}
+li {
+  display: inline-block;
+}
+
+</style>
+ -->
+
 <script>
   import Heart_1 from './heart.svelte';
    
@@ -19,7 +92,7 @@
 	}
 </script>
 
-<ul>
+<!-- <ul> -->
 
 {#await promise}
 	<strong>...Loading</strong>
@@ -63,7 +136,7 @@
   <Heart_1 count={heart[0].AB_POSITIVE} count_id={6} label={'AB+'}></Heart_1>
 
   </li>
-
+  
 
   <li>
 
@@ -82,10 +155,8 @@
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}
-</ul>
+<!-- </ul> -->
 <style>
-
-
 ul {
     
         display: -webkit-box;
@@ -108,4 +179,6 @@ li {
   max-height: 10vh;
   padding:35px ;
 }
+
+
 </style>
